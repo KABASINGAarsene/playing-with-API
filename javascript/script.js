954b3d1d59d437eee3e6fd0ecdd2244a10945738
KABASINGAarsene <a.kabasinga@alustudent.com>
2025-03-29 14:09:49 +0200

    Second changes

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+
+
+
+// Function to toggle the navigation menu visibility
+function toggleMenu() {
+    const menu = document.getElementById("nav-links");
+    menu.classList.toggle("active");
+}
+
+
+
+
